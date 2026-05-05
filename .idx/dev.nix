85a65a2fd921c9c8113bb77dfc12b088ffe24b23
parkjunhyoung0528 <parkjunhyoung0528@naver.com>
2026-05-05 13:44:21 +0000

    feat: 작업지시서 PDF 기반 정밀 세척 공정 상세 페이지 구축

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,6 +2,7 @@
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
+
   channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
